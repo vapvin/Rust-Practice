20ecd5ca88dce3902a7030ba1bd24af40455ed43
vapvin <vins@vins.dev>
2020-11-15 11:42:01 +0900

    Create Random Number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
    println!("숫자를 맞추어 주세요");
+
+   let secret_number = rand::thread_rng().gen_range(1, 101);
+
+   println!("랜덤 숫자 {}", secret_number);
 
    println!("생각하는 정답을 입력해 주세요");
 
@@ -10,4 +15,5 @@ fn main() {
    io::stdin().read_line(&mut guess)
        .expect("값을 읽어오지 못 했습니다.");
    println!("입력한 값: {}", guess);
+   
 }
