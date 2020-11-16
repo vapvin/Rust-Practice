4a94ceee283dced8744d2183d01684551bd6d85a
vapvin <vins@vins.dev>
2020-11-16 23:51:07 +0900

    Loop

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,23 @@ fn main() {
 
    println!("랜덤 숫자 {}", secret_number);
 
-   println!("생각하는 정답을 입력해 주세요");
+   loop {
+    println!("생각하는 정답을 입력해 주세요");
 
-   let mut guess = String::new();
-
-   io::stdin().read_line(&mut guess)
-       .expect("값을 읽어오지 못 했습니다.");
-   let guess: u32 = guess.trim().parse()
-       .expect("입력한 값이 숫자가 아닙니다.");
-   println!("입력한 값: {}", guess);
-   match guess.cmp(&secret_number){
-       Ordering::Less => println!("입력한 숫자가 정답보다 작습니다."),
-       Ordering::Greater => println!("입력한 숫자가 정다봅다 높습니다."),
-       Ordering::Equal => println!("정답입니다"),
+    let mut guess = String::new();
+ 
+    io::stdin().read_line(&mut guess)
+        .expect("값을 읽어오지 못 했습니다.");
+    let guess: u32 = guess.trim().parse()
+        .expect("입력한 값이 숫자가 아닙니다.");
+    println!("입력한 값: {}", guess);
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("입력한 숫자가 정답보다 작습니다."),
+        Ordering::Greater => println!("입력한 숫자가 정다봅다 높습니다."),
+        Ordering::Equal => {
+            println!("정답입니다");
+            break;
+        },
+    }
    }
 }
